45fe29484dabea09ee3df468bba9cf6d7665d303
Amanuel Zeleke <amanuel.zeleke@nivohub.com>
2024-05-12 20:58:12 +0100

    Refactor DockerController and add connectDocker method
    The DockerController was refactored to initialize the DockerModel and DockerInteractor with a UserModel. A private connectDocker method was introduced which manages the connection to the Docker server asynchronously and handles potential Docker not running exceptions.

--- src/main/java/nivohub/devinspector/controller/DockerController.java
+++ src/main/java/nivohub/devinspector/controller/DockerController.java
@@ -1,21 +1,44 @@
 package nivohub.devinspector.controller;
 
+import javafx.concurrent.Task;
 import javafx.scene.layout.Region;
 import javafx.util.Builder;
+import nivohub.devinspector.exceptions.DockerNotRunningException;
 import nivohub.devinspector.interactor.DockerInteractor;
 import nivohub.devinspector.model.DockerModel;
+import nivohub.devinspector.model.UserModel;
 import nivohub.devinspector.view.DockerViewBuilder;
 
 public class DockerController {
 
     private final DockerInteractor interactor;
     private final Builder<Region> viewBuilder;
-    private final DockerModel model;
 
-    public DockerController() {
-        model = new DockerModel();
-        interactor = new DockerInteractor(model);
-        viewBuilder = new DockerViewBuilder(model);
+    public DockerController(UserModel userModel) {
+        DockerModel model = new DockerModel();
+        interactor = new DockerInteractor(model, userModel);
+        viewBuilder = new DockerViewBuilder(model, this::startContainerAction, this::connectDocker);
+    }
+
+    private void connectDocker() {
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws DockerNotRunningException {
+                interactor.connectDocker();
+                return null;
+            }
+        };
+        task.setOnSucceeded(e -> {
+            interactor.updateModelConnection();
+        });
+        task.setOnFailed(e -> {
+            interactor.addToOutput(e.getSource().getException().getMessage());
+    });
+        new Thread(task).start();
+    }
+
+    private void startContainerAction() {
+
     }
 
     public Region getView(){
