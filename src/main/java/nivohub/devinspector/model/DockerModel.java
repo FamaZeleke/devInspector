3d017a4e24ab60d7066d45e23a620479fad64efa
Amanuel Zeleke <amanuel.zeleke@nivohub.com>
2024-05-12 17:18:12 +0100

    Updated DockerModel with new properties and listeners
    Enhanced the DockerModel class by declaring new SimpleStringProperty, ListProperty, and BooleanBinding variables. Set up listeners to enable dynamic updates based on the selectedImage's changes. These changes facilitate better management and interaction with Docker images and their associated tags.

--- src/main/java/nivohub/devinspector/model/DockerModel.java
+++ src/main/java/nivohub/devinspector/model/DockerModel.java
@@ -1,5 +1,9 @@
 package nivohub.devinspector.model;
 
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import nivohub.devinspector.docker.DockerImage;
@@ -8,7 +12,11 @@ import java.util.stream.Collectors;
 
 public class DockerModel {
 
-    private ObservableList<DockerImage> dockerImages;
+    private final ObservableList<DockerImage> dockerImages;
+    private final SimpleStringProperty selectedImage = new SimpleStringProperty();
+    private final ListProperty<String> selectedImageTags = new SimpleListProperty<>();
+    private final SimpleStringProperty selectedTag = new SimpleStringProperty();
+    private final BooleanBinding imageSelected;
 
     public DockerModel() {
         this.dockerImages = FXCollections.observableArrayList(
@@ -16,6 +24,26 @@ public class DockerModel {
                 new DockerImage("ubuntu", FXCollections.observableArrayList("latest","20.04")),
                 new DockerImage("jupyter/base-notebook", FXCollections.observableArrayList("latest","arm64"))
         );
+        imageSelected = selectedImage.isNotNull();
+        selectedImage.addListener((observable, oldValue, newValue) -> {
+            DockerImage image = dockerImages.stream()
+                    .filter(dockerImage -> dockerImage.imageName().equals(newValue))
+                    .findFirst()
+                    .orElse(null);
+            if (image != null) {
+                selectedImageTags.set(FXCollections.observableArrayList(image.tags()));
+            } else {
+                selectedImageTags.set(FXCollections.observableArrayList());
+            }
+        });
+    }
+
+    public SimpleStringProperty selectedImageProperty() {
+        return selectedImage;
+    }
+
+    public SimpleStringProperty selectedTagProperty() {
+        return selectedTag;
     }
 
     public ObservableList<DockerImage> getDockerImages() {
@@ -26,6 +54,14 @@ public class DockerModel {
         return dockerImages.stream()
                 .map(DockerImage::imageName)
                 .collect(Collectors.toCollection(FXCollections::observableArrayList));
+    }
+
+    public ListProperty<String> selectedImageTagsProperty() {
+        return selectedImageTags;
+    }
+
+    public BooleanBinding imageSelectedProperty() {
+        return imageSelected;
     }
 
     public void addDockerImage(DockerImage dockerImage) {
