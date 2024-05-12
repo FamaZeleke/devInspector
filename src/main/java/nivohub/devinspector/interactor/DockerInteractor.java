4c885f0c590c086ab1564f8bbb27c9a0eeeb81a0
Amanuel Zeleke <amanuel.zeleke@nivohub.com>
2024-05-12 20:57:47 +0100

    Expanded DockerInteractor with connect and output methods
    Enhanced DockerInteractor class by adding docker connection, log streaming and model update methods. These changes allow the application to interact with Docker engines more proficiently, handling exceptions, streaming logs and updating connection status.

--- src/main/java/nivohub/devinspector/interactor/DockerInteractor.java
+++ src/main/java/nivohub/devinspector/interactor/DockerInteractor.java
@@ -1,11 +1,43 @@
 package nivohub.devinspector.interactor;
 
+import javafx.application.Platform;
+import nivohub.devinspector.docker.DockerEngine;
+import nivohub.devinspector.exceptions.DockerNotRunningException;
 import nivohub.devinspector.model.DockerModel;
+import nivohub.devinspector.model.UserModel;
 
 public class DockerInteractor {
     private final DockerModel model;
+    private final DockerEngine dockerEngine;
 
-    public DockerInteractor(DockerModel model) {
+    public DockerInteractor(DockerModel model, UserModel userModel) {
         this.model = model;
+        this.dockerEngine = new DockerEngine(userModel.getPlatform());
+    }
+
+    public void connectDocker() throws DockerNotRunningException {
+        try {
+            dockerEngine.createDockerClient();
+        } catch (Exception e) {
+            throw new DockerNotRunningException(e.getMessage());
+        }
+    }
+
+    public void updateModelConnection() {
+        model.dockerConnectedProperty().set(true);
+    }
+
+    public void addToOutput (String message) {
+        model.addToOutput(message);
+    }
+
+    public void streamLogs(String containerId) {
+        new Thread(() -> {
+            dockerEngine.streamContainerLogs(containerId, logMessage -> {
+                Platform.runLater(() -> {
+                    model.addToOutput(logMessage);
+                });
+            });
+        }).start();
     }
 }
