8d29e14ad4d9039bf8ce5f588509acbfb661cca2
Amanuel Zeleke <amanuel.zeleke@nivohub.com>
2024-05-12 15:38:48 +0100

    Add DockerInteractor class in the interactor package
    A new DockerInteractor class has been introduced within the interactor package. This class will act as the intermediary between the DockerModel and the rest of the application.

---
+++ src/main/java/nivohub/devinspector/interactor/DockerInteractor.java
@@ -0,0 +1,11 @@
+package nivohub.devinspector.interactor;
+
+import nivohub.devinspector.model.DockerModel;
+
+public class DockerInteractor {
+    private final DockerModel model;
+
+    public DockerInteractor(DockerModel model) {
+        this.model = model;
+    }
+}
