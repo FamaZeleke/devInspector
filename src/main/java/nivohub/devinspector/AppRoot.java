beb23a450fc1cb638343e0421e754a1009ebf60c
Amanuel Zeleke <amanuel.zeleke@nivohub.com>
2024-05-13 13:27:10 +0100

    Removed a commented-out start method

--- src/main/java/nivohub/devinspector/AppRoot.java
+++ src/main/java/nivohub/devinspector/AppRoot.java
@@ -30,6 +30,7 @@ public class AppRoot extends Application implements StageManager {
         String platform = osName.contains("mac") ? "mac" : "windows";
         userModel.setPlatform(platform);
         userModel.setOsArch(osArch);
+        // Dependency Injection via compositon - Guice Might be a good idea
         this.applicationController = new ApplicationController(userModel, this);
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
         LoginController loginController = new LoginController(userModel, applicationController);
@@ -44,29 +45,5 @@ public class AppRoot extends Application implements StageManager {
         primaryStage.setHeight(768);
     }
 
-
-//    @Override
-//    public void start(Stage primaryStage) {
-//
-//        String osArch = System.getProperty("os.arch");
-//        String osName = System.getProperty("os.name").toLowerCase();
-//        String platform = osName.contains("mac") ? "mac" : "windows";
-//
-//
-//        SceneController sceneController = new SceneController(primaryStage);
-//        CommandLineController commandLineController = new CommandLineController(platform);
-//
-//        LoginScene loginScene = new LoginScene();
-//        User user = new User(osArch, platform);
-//        LoginController loginController = new LoginController(loginScene, user, sceneController, commandLineController);
-//        loginScene.setController(loginController);
-//        loginScene.setSubmitAction();
-//
-//        primaryStage.setScene(loginScene.createScene());
-//        primaryStage.setTitle("DevInspector");
-//        primaryStage.show();
-//
-//        Platform.runLater(primaryStage::requestFocus);
-//    }
 
 }
