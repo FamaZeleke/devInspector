fe81121eb4d0d229ccae9ca65eee1f750e049966
Amanuel Zeleke <amanuel.zeleke@nivohub.com>
2024-05-14 20:05:24 +0100

    Expand DockerInterface to include start/stop/remove methods for containers

--- src/main/java/nivohub/devinspector/interfaces/DockerInterface.java
+++ src/main/java/nivohub/devinspector/interfaces/DockerInterface.java
@@ -3,5 +3,9 @@ package nivohub.devinspector.interfaces;
 public interface DockerInterface {
       void connectDocker();
       void disconnectDocker();
+        void startContainer(String containerId);
+        void stopContainer(String containerId);
+
+      void removeContainer(String containerId);
       //If I wanted to grow this application out to include more features, used across my other views, I would add more methods here.
 }
