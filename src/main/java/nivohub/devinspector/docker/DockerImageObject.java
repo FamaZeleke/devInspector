08bad8c21f9c6abf119c4b9bf66613e790149f0e
Amanuel Zeleke <amanuel.zeleke@nivohub.com>
2024-05-15 01:55:08 +0100

    Enhanced POJO for DockerImageObject

--- src/main/java/nivohub/devinspector/docker/DockerImageObject.java
+++ src/main/java/nivohub/devinspector/docker/DockerImageObject.java
@@ -2,12 +2,25 @@ package nivohub.devinspector.docker;
 
 public class DockerImageObject {
 
-private final String imageName;
-private final String[] tags;
+    private final String imageName;
+    private final String[] tags;
+    private final String imageId;
+    private final String architecture;
+    private final String os;
+    private final String container;
 
-    public DockerImageObject(String imageName, String[] tags) {
+    public DockerImageObject(String imageId, String imageName, String[] tags, String architecture, String os, String container) {
+        this.imageId = imageId;
         this.imageName = imageName;
         this.tags = tags;
+        this.architecture = architecture;
+        this.os = os;
+        this.container = container;
+    }
+
+    // Overloaded constructor for default images
+    public DockerImageObject(String imageName, String[] tags) {
+        this(null, imageName, tags, null, null, null);
     }
 
     public String imageName() {
