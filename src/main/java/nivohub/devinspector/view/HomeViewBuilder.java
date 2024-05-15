85f547d499f426b5e47fa7522e8e52ae31c7467f
Amanuel Zeleke <amanuel.zeleke@nivohub.com>
2024-05-15 02:43:38 +0100

    Refactor HomeViewBuilder to use DockerContainerObject and DockerImageObject, improve UI functionality
    - Updated DockerContainer references to DockerContainerObject throughout HomeViewBuilder.
    - Introduced DockerImageObject and implemented an image table in the "Images" tab:
      - Created columns for ID, Name, Tags, Container, Architecture, and OS with copy-to-clipboard functionality.
    - Enhanced container table in the "Containers" tab:
      - Added columns for ID, Name, Image, Status, Host Port, Exposed Port, Toggle, and Remove.
      - Improved the Toggle column with buttons to start/stop containers and Remove column with buttons to remove containers.
      - Status column dynamically updates to show "Running" or "Stopped" based on container status.
    - Implemented reusable methods for creating table columns with copy-to-clipboard functionality.
    - Streamlined context menus for table cells to enhance user interaction.
    - Added detailed Javadoc comments for better code maintainability and readability.
    - Organized import statements and ensured consistency across the codebase.

--- src/main/java/nivohub/devinspector/view/HomeViewBuilder.java
+++ src/main/java/nivohub/devinspector/view/HomeViewBuilder.java
@@ -3,12 +3,16 @@ package nivohub.devinspector.view;
 import javafx.geometry.Side;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.Region;
 import javafx.util.Builder;
 import javafx.util.Callback;
-import nivohub.devinspector.docker.DockerContainer;
+import nivohub.devinspector.docker.DockerContainerObject;
+import nivohub.devinspector.docker.DockerImageObject;
 import nivohub.devinspector.interfaces.DockerInterface;
 import nivohub.devinspector.model.DockerModel;
+import org.jetbrains.annotations.NotNull;
 
 public class HomeViewBuilder implements Builder<Region> {
 
@@ -36,123 +40,215 @@ private final DockerModel model;
 
     private Tab createImageTab() {
         Tab imageTab = new Tab("Images");
+        imageTab.setContent(createImageTable());
         return imageTab;
     }
 
-    private TableView<DockerContainer> createContainerTable() {
-        TableView<DockerContainer> table = new TableView<>();
+    // Create a table view for the Docker images
+    private TableView<DockerImageObject> createImageTable() {
+        TableView<DockerImageObject> table = new TableView<>();
+        table.setItems(model.getDockerImages());
+        table.autosize();
 
-        table.setItems(model.getRunningContainers());
+        TableColumn<DockerImageObject, String> idColumn = createCopyableColumn("ID", "imageId", 250);
 
-        TableColumn<DockerContainer, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("containerName"));
-        nameColumn.setPrefWidth(200);
+        TableColumn<DockerImageObject, String> nameColumn = createCopyableColumn("Name", "imageName", 150);
 
-        TableColumn<DockerContainer, String> imageColumn = new TableColumn<>("Image");
-        imageColumn.setCellValueFactory(new PropertyValueFactory<>("image"));
-        imageColumn.setPrefWidth(150);
+        TableColumn<DockerImageObject, String> tagsColumn = createCopyableColumn("Tags", "tagsAsString", 150);
 
-        TableColumn<DockerContainer, Boolean> statusColumn = new TableColumn<>("Status");
-        statusColumn.setCellValueFactory(cellData -> cellData.getValue().runningProperty());
-        statusColumn.setCellFactory(column -> new TableCell<>() {
-            @Override
-            protected void updateItem(Boolean item, boolean empty) {
-                super.updateItem(item, empty);
+        TableColumn<DockerImageObject, String> containerColumn = createCopyableColumn("Container", "container", 150);
 
-                if (item == null || empty) {
-                    setText(null);
-                } else {
-                    setText(item ? "Running" : "Stopped");
-                }
-            }
-        });
+        TableColumn<DockerImageObject, String> architectureColumn = createColumn("Arch", "architecture", 150);
 
-        TableColumn<DockerContainer, String> cpuColumn = new TableColumn<>("CPU %");
-        cpuColumn.setCellValueFactory(new PropertyValueFactory<>("cpu"));
+        TableColumn<DockerImageObject, String> osColumn = createColumn("OS", "os", 150);
 
-        TableColumn<DockerContainer, String> hostPortColumn = new TableColumn<>("Host Port");
-        hostPortColumn.setCellValueFactory(new PropertyValueFactory<>("hostPort"));
-        hostPortColumn.setPrefWidth(100);
 
-        TableColumn<DockerContainer, String> exposedPortColumn = new TableColumn<>("Exposed Port");
-        exposedPortColumn.setCellValueFactory(new PropertyValueFactory<>("exposedPort"));
-        exposedPortColumn.setPrefWidth(100);
+        table.getColumns().add(idColumn);
+        table.getColumns().add(nameColumn);
+        table.getColumns().add(tagsColumn);
+        table.getColumns().add(containerColumn);
+        table.getColumns().add(architectureColumn);
+        table.getColumns().add(osColumn);
+        return table;
+    }
 
-        TableColumn<DockerContainer, Void> toggleColumn = new TableColumn<>("Toggle");
-        Callback<TableColumn<DockerContainer, Void>, TableCell<DockerContainer, Void>> toggleCellFactory =
-                param -> new TableCell<>() {
-                    private final Button btn = new Button();
+    // Create a table view for the Docker containers
+    private TableView<DockerContainerObject> createContainerTable() {
+        TableView<DockerContainerObject> table = new TableView<>();
+        table.setItems(model.getDockerContainers());
+        table.autosize();
 
-                    {
-                        btn.setOnAction(event -> {
-                            DockerContainer data = getTableView().getItems().get(getIndex());
-                            if (data.runningProperty().get()) {
-                                dockerInterface.stopContainer(data.getContainerId());
-                            } else {
-                                dockerInterface.startContainer(data.getContainerId());
-                            }
-                        });
-                    }
+        TableColumn<DockerContainerObject, String> idColumn = createCopyableColumn("ID", "containerId", 200);
 
-                    @Override
-                    public void updateItem(Void item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                        } else {
-                            DockerContainer data = getTableView().getItems().get(getIndex());
-                            btn.setText(data.runningProperty().get() ? "Stop" : "Start");
-                            setGraphic(btn);
-                        }
-                    }
-                };
-        toggleColumn.setCellFactory(toggleCellFactory);
+        TableColumn<DockerContainerObject, String> nameColumn = createCopyableColumn("Name", "containerName", 200);
 
-        TableColumn<DockerContainer, Void> removeColumn = new TableColumn<>("Remove");
-        Callback<TableColumn<DockerContainer, Void>, TableCell<DockerContainer, Void>> removeCellFactory =
-                param -> new TableCell<>() {
-                    private final Button btn = new Button("Remove");
+        TableColumn<DockerContainerObject, String> imageColumn = createCopyableColumn("Image", "image", 200);
 
-                    {
-                        btn.setOnAction(event -> {
-                            DockerContainer data = getTableView().getItems().get(getIndex());
-                            dockerInterface.removeContainer(data.getContainerId());
-                        });
-                    }
+        TableColumn<DockerContainerObject, Boolean> statusColumn = createStatusColumn();
 
-                    @Override
-                    public void updateItem(Void item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                        } else {
-                            setGraphic(btn);
-                        }
-                    }
-                };
-        removeColumn.setCellFactory(removeCellFactory);
+        TableColumn<DockerContainerObject, String> hostPortColumn = createColumn("Host Port", "hostPort", 100);
 
+        TableColumn<DockerContainerObject, String> exposedPortColumn = createColumn("Exposed Port", "exposedPort", 100);
+
+        TableColumn<DockerContainerObject, Void> toggleColumn = createToggleColumn();
+
+        TableColumn<DockerContainerObject, Void> removeColumn = createRemoveColumn();
+
+        table.getColumns().add(idColumn);
         table.getColumns().add(nameColumn);
         table.getColumns().add(imageColumn);
         table.getColumns().add(statusColumn);
-        table.getColumns().add(cpuColumn);
         table.getColumns().add(hostPortColumn);
         table.getColumns().add(exposedPortColumn);
         table.getColumns().add(toggleColumn);
         table.getColumns().add(removeColumn);
 
-        // TODO: Add table to your layout
         return table;
     }
 
-    private void createImageTable() {
-
+    private <T> TableColumn<T, String> createColumn(String name, String property, int width) {
+        TableColumn<T, String> column = new TableColumn<>(name);
+        column.setCellValueFactory(new PropertyValueFactory<>(property));
+        column.setPrefWidth(width);
+        return column;
     }
 
-    private Region styledTable() {
-        Region table = new TableView<DockerContainer>();
-        return table;
+    private <T> TableColumn<T, String> createCopyableColumn(String name, String property, int width) {
+        TableColumn<T, String> column = new TableColumn<>(name);
+        column.setCellValueFactory(new PropertyValueFactory<>(property));
+        column.setPrefWidth(width);
+
+        column.setCellFactory(tc -> {
+            TableCell<T, String> cell = new TableCell<>();
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem copyMenuItem = new MenuItem("Copy");
+            copyMenuItem.setOnAction(e -> {
+                final Clipboard clipboard = Clipboard.getSystemClipboard();
+                final ClipboardContent content = new ClipboardContent();
+                content.putString(cell.getItem());
+                clipboard.setContent(content);
+            }); //Set the action of the item to copy the cell's content to the clipboard
+            contextMenu.getItems().add(copyMenuItem);
+            cell.textProperty().bind(cell.itemProperty());
+            cell.setContextMenu(contextMenu);
+            return cell;
+        });
+
+        return column;
     }
 
+    private TableColumn<DockerContainerObject, Void> createRemoveColumn() {
+        TableColumn<DockerContainerObject, Void> removeColumn = new TableColumn<>("Remove");
+        Callback<TableColumn<DockerContainerObject, Void>, TableCell<DockerContainerObject, Void>> removeCellFactory =
+                param -> new TableCell<>() {
+                    private final Button action = new Button("Remove");
+
+                    {
+                        action.setOnAction(event -> { //Set the action of the button to remove the container
+                            DockerContainerObject data = getTableView().getItems().get(getIndex());
+                            dockerInterface.removeContainer(data.getContainerId());
+                        });
+                    }
+
+                    /**
+                     * Updates the item in the cell.
+                     * If the cell is empty, it sets the graphic to null.
+                     * If the cell is not empty, it sets the graphic of the cell to the button.
+                     *
+                     * @param item - The item to update (not used as this is a Void column).
+                     * @param empty - Whether the cell is empty.
+                     */
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(action);
+                        }
+                    }
+                };
+        removeColumn.setCellFactory(removeCellFactory);
+        removeColumn.setPrefWidth(100);
+        return removeColumn;
+    }
+
+    private TableColumn<DockerContainerObject, Void> createToggleColumn() {
+        TableColumn<DockerContainerObject, Void> toggleColumn = new TableColumn<>("Toggle");
+
+        toggleColumn.setCellFactory(param -> new TableCell<>() {
+            private final Button action = new Button();
+
+            {
+                // Set the action of the button to toggle the container state
+                action.setOnAction(event -> toggleContainerState());
+            }
+            /**
+             * Toggles the running state of the Docker container.
+             * If the container is running, it stops the container.
+             * If the container is not running, it starts the container.
+             */
+            private void toggleContainerState() {
+                DockerContainerObject data = getTableView().getItems().get(getIndex());
+                if (data.runningProperty().get()) {
+                    dockerInterface.stopContainer(data.getContainerId());
+                } else {
+                    dockerInterface.startContainer(data.getContainerId());
+                }
+            }
+
+            /**
+             * Updates the item in the cell.
+             * If the cell is empty, it sets the graphic to null.
+             * If the cell is not empty, it sets the text of the button to "Stop" if the container is running, or "Start" if it's not.
+             * It also sets the graphic of the cell to the button.
+             *
+             * @param item - The item to update (not used as this is a Void column).
+             * @param empty - Whether the cell is empty.
+             */
+            @Override
+            public void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    DockerContainerObject data = getTableView().getItems().get(getIndex());
+                    action.setText(data.runningProperty().get() ? "Stop" : "Start"); // Set the text of the button to "Stop" or "Start" based on the running state
+                    setGraphic(action);
+                }
+            }
+        });
+
+        return toggleColumn;
+    }
+    
+    private TableColumn<DockerContainerObject, Boolean> createStatusColumn() {
+        TableColumn<DockerContainerObject, Boolean> statusColumn = new TableColumn<>("Status");
+        statusColumn.setCellValueFactory(cellData -> cellData.getValue().runningProperty()); // Bind the running property to the column
+
+        statusColumn.setCellFactory(column -> new TableCell<>() {
+            /**
+             * Updates the item in the cell.
+             * If the cell is empty, it sets the text to null.
+             * If the cell is not empty, it checks the item (running status) and sets the text to "Running" or "Stopped".
+             *
+             * @param item - The item to update (running status of the Docker container).
+             * @param empty - Whether the cell is empty.
+             */
+            @Override
+            protected void updateItem(Boolean item, boolean empty) {
+                super.updateItem(item, empty);
+                String text;
+                if (empty) {
+                    text = null;
+                } else {
+                    text = item ? "Running" : "Stopped";
+                }
+                setText(text);
+            }
+        });
+        statusColumn.setPrefWidth(100);
+        return statusColumn;
+    }
 
 }
